d47635920cd9a2303957c6d5b94cce5643496e2b
Alin Lupascu <lupascu.marius@gmail.com>
2018-09-11 13:06:58 -0600

    Add files via upload

---
+++ SlackInstall.ps1
@@ -0,0 +1,21 @@
+﻿$slackBaseDir = "$env:LocalAppData\slack\"
+$installations = Get-ChildItem $slackBaseDir -Directory | Where-Object { $_.Name.StartsWith("app-") }
+$version = $installations | Sort-Object { [version]$_.Name.Substring(4) } | Select-Object -Last 1
+Write-Output "Choosing highest present Slack version: $version"
+ $customContent = @'
+
+
+ document.addEventListener('DOMContentLoaded', function() {
+ $.ajax({
+   url: 'https://raw.githubusercontent.com/alienator88/slacky-dark/master/dark.css',
+   success: function(css) {
+     $("<style></style>").appendTo('head').html(css);
+   }
+ });
+});
+
+
+'@
+Add-Content "$($version.FullName)\resources\app.asar.unpacked\src\static\ssb-interop.js" $customContent
+ Write-Output "SlackyDark has been installed - please restart Slack"
+ Read-Host
