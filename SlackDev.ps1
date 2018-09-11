932e8e77cfca6d200ae9ecfecac6c59e79b22a05
Alin Lupascu <lupascu.marius@gmail.com>
2018-09-11 13:35:16 -0600

    Add files via upload

---
+++ SlackDev.ps1
@@ -0,0 +1,7 @@
+﻿
+# Set environment variable
+[System.Environment]::SetEnvironmentVariable('SLACK_DEVELOPER_MENU', 'true', 'Process')
+
+ # Launch Slack (replace x.y.z with the latest version)
+& $env:LOCALAPPDATA\slack\app-3.3.1\slack.exe
+ # Open developer console by pressing: Ctrl + Alt + I
